13a08a32a717a09e2a8e59221c2c33e0a50f5a1f
Zizhun Guo <zg2808@g.rit.edu>
2017-03-30 14:22:38 -0400

    Add files via upload

---
+++ NAudioTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace APITest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            var soundFile = "QHC.wav";
+            var wfr = new WaveFileReader(soundFile);
+            WaveChannel32 wc = new WaveChannel32(wfr);
+            var audioOutput = new DirectSoundOut();
+
+            wfr.Read(data, 0, 100);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+
+            audioOutput.Init(wc);
+            audioOutput.Play();
+
+            while (true) ;
+        }
+    }
+}
